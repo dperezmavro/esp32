1c9a3c8400b056e4ec4a73f51a7127843e60997d
dperezmavro <dio.pm@live.com>
2025-05-04 17:44:07 +0100

    added successful receiver poc

--- wireless-keyboard/wireless_keyboard_receiver/wireless_keyboard_receiver.ino
+++ wireless-keyboard/wireless_keyboard_receiver/wireless_keyboard_receiver.ino
@@ -4,26 +4,35 @@
 #include <FS.h>
 #include <SD.h>
 #include <SPI.h>
-
-// #include <USBHID.h>
-// #include "USBHIDKeyboard.h"
-// USBHIDKeyboard Keyboard;
-
+#include "USB.h"
+#include "USBHIDKeyboard.h"
 // include common functionality
 #include "../common.h"
 #include "stratagems.h"
+
+#define WAIT_BETWEEN_KEY_PRESS 75
+
+// USB Keyboard
+USBHIDKeyboard Keyboard;
+
 
 const char *stratagems;
 StratagemManager *sm;
 data_packet command;
 
+// create debouncer
+long lastSwitchDetectedMIllis;
+const long multiTriggerPreventionInterval = 300;
+
 void setup() {
   // serial for output
   Serial.begin(115200);
 
+  // debouncer
+  lastSwitchDetectedMIllis = millis();
+
   // LED for feedBACK
   pinMode(LED_BUILTIN, OUTPUT);
-
   /*
   
   ESP NOW AND WIFI SETUP
@@ -66,15 +75,29 @@ void setup() {
 
   stratagems = readFile(SD, STRATAGEMS_FILE);
 
+
   /*
           Initialise stratagem manager 
   */
   sm = new StratagemManager(stratagems);
+
+  /**
+  Keyboard Setup
+  */
+  // Initialize USB HID Keyboard
+  Keyboard.begin();
+  USB.begin();
 }
 
 // callback function that will be executed when data is received
 void OnDataRecv(const uint8_t *mac, const uint8_t *incomingData, int len) {
+  /**
+  USB EXPERIMENT
+  */
+  Serial.println("printed");
+  Keyboard.write('A');
   blink_led();
+  /*
   memcpy(&command, incomingData, sizeof(command));
   auto stgm = sm->get_stratagem(command.button_number, command.pin_position);
   Serial.printf(
@@ -84,10 +107,7 @@ void OnDataRecv(const uint8_t *mac, const uint8_t *incomingData, int len) {
     command.pin_position,
     stgm.c_str(),
     stgm.length());
-
-  // Keyboard.print("You pressed the button ");
-  // Keyboard.print(command.button_number);
-  // Keyboard.println(".");
+  */
 }
 
 
@@ -109,6 +129,50 @@ const char *readFile(fs::FS &fs, const char *path) {
   file.close();
 
   return stratagems.c_str();
+}
+
+void callStratagem(char primaryStratagem[], char secondaryStratagem[], int mode_pin) {
+  if (millis() - lastSwitchDetectedMIllis < multiTriggerPreventionInterval) {
+#ifdef DEBUG
+    Serial.println("spotted multiple bounces");
+#endif
+    return;
+  }
+
+  lastSwitchDetectedMIllis = millis();
+
+  digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
+  delay(50);                        // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
+
+  int p = digitalRead(mode_pin);
+  char *s = p == 1 ? primaryStratagem : secondaryStratagem;
+
+#ifdef DEBUG
+  Serial.print(p);
+  Serial.print(" ");
+  Serial.println(s);
+  return;
+#endif
+
+  Keyboard.press(KEY_LEFT_CTRL);
+  delay(WAIT_BETWEEN_KEY_PRESS);
+
+  for (int i = 0; i < strlen(s); i++) {
+    char c = s[i];
+
+    Keyboard.press(c);
+    delay(WAIT_BETWEEN_KEY_PRESS);
+    Keyboard.release(c);
+    delay(WAIT_BETWEEN_KEY_PRESS);
+  }
+
+  // TODO (dio): figure out mouse library
+  // Mouse.press();
+  // delay(WAIT_BETWEEN_KEY_PRESS);
+  // Mouse.release();
+
+  Keyboard.releaseAll();
 }
 
 // needs to do nothing actively
