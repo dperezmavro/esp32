a3323234933188dfb0b03a36fd38749d1300ab9e
dio-trinny <dio@trinnylondon.com>
2025-08-19 10:32:02 +0100

    cleanup

--- wireless-keyboard/common.h
+++ wireless-keyboard/common.h
@@ -1,18 +1,9 @@
 #ifndef _COMMON_H
 #define _COMMON_H
 
-#define LED_BUILTIN 48
-#define LED_BLINK_DURATION 250
-
 struct data_packet {
   uint8_t button_number;
   uint8_t pin_position;
 };
-
-void blink_led() {
-    digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
-    delay(LED_BLINK_DURATION);                       // wait for a second
-    digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
-}
 
 #endif
