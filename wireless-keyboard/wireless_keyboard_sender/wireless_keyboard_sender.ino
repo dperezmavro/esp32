582ca003331be97b8175b42cc866aca73d1a4141
dio-trinny <dio@trinnylondon.com>
2025-08-24 16:20:55 +0100

    cleanup

--- wireless-keyboard/wireless_keyboard_sender/wireless_keyboard_sender.ino
+++ wireless-keyboard/wireless_keyboard_sender/wireless_keyboard_sender.ino
@@ -5,10 +5,9 @@
 #include "./common.h"
 #include "./setup.h"
 
-// #define BUTTON_PIN_0 12
-// #define BUTTON_TOGGLE_0 14
+#define BUTTON_PIN_0 12
+#define BUTTON_TOGGLE_0 14
 
-// #define BUTTON_PIN_1 15
 #define BUTTON_PIN_1 4
 #define BUTTON_TOGGLE_1 5
 
@@ -18,8 +17,8 @@ void setup()
   Serial.begin(115200);
 
   // button setup
-  // pinMode(BUTTON_PIN_0, INPUT_PULLUP);
-  // pinMode(BUTTON_TOGGLE_0, INPUT_PULLUP);
+  pinMode(BUTTON_PIN_0, INPUT_PULLUP);
+  pinMode(BUTTON_TOGGLE_0, INPUT_PULLUP);
 
   pinMode(BUTTON_PIN_1, INPUT_PULLUP);
   pinMode(BUTTON_TOGGLE_1, INPUT_PULLUP);
@@ -33,23 +32,21 @@ void setup()
 
 void loop()
 {
-  // bool button_0 = digitalRead(BUTTON_PIN_0);     // Read switch
-  // bool toggle_switch_0 = digitalRead(BUTTON_TOGGLE_0);  // Read toggle
-  // if (button_0 == LOW)
-  // {
-  //   Serial.printf("clicked button %d: %d\n", button_0, toggle_switch_0);
-  //   command.button_number = 0;
-  //   command.pin_position  = toggle_switch_0;
-  //   send_command(0, toggle_switch_0);
-  // }
+  bool button_0 = digitalRead(BUTTON_PIN_0);     // Read switch
+  if (button_0 == LOW)
+  {
+    bool toggle_switch_0 = digitalRead(BUTTON_TOGGLE_0);  // Read toggle
+    
+    Serial.printf("clicked button %d: %d\n", button_0, toggle_switch_0);
+    send_command(0, toggle_switch_0);
+  }
 
   bool button_1        = digitalRead(BUTTON_PIN_1);     // Read switch
-  bool toggle_switch_1 = digitalRead(BUTTON_TOGGLE_1);  // Read toggle
   if (button_1 == LOW)
   {
+    bool toggle_switch_1 = digitalRead(BUTTON_TOGGLE_1);  // Read toggle
+    
     Serial.printf("clicked button %d: %d\n", button_1, toggle_switch_1);
     send_command(1, toggle_switch_1);
   }
-
-  // delay(50);
 }
