5d108fb024b7ae040d5aa1e0cb26acda22ccec49
dio-trinny <dio@trinnylondon.com>
2025-08-24 13:53:52 +0100

    debugging buttons

--- wireless-keyboard/wireless_keyboard_sender/wireless_keyboard_sender.ino
+++ wireless-keyboard/wireless_keyboard_sender/wireless_keyboard_sender.ino
@@ -5,8 +5,8 @@
 #include "./common.h"
 #include "./setup.h"
 
-#define BUTTON_PIN_1 14
-#define BUTTON_TOGGLE_1 12
+#define BUTTON_PIN_1 12
+#define BUTTON_TOGGLE_1 14
 
 #define BUTTON_PIN_2 15
 #define BUTTON_TOGGLE_2 13
@@ -20,6 +20,9 @@ void setup()
   pinMode(BUTTON_PIN_1, INPUT_PULLUP);
   pinMode(BUTTON_TOGGLE_1, INPUT_PULLUP);
 
+  pinMode(BUTTON_PIN_2, INPUT_PULLUP);
+  pinMode(BUTTON_TOGGLE_2, INPUT_PULLUP);
+
   int err_esp_now = setuip_esp_now();
   if (!err_esp_now)
   {
@@ -31,10 +34,6 @@ void loop()
 {
   bool switchState1 = digitalRead(BUTTON_PIN_1);     // Read switch
   bool toggleState1 = digitalRead(BUTTON_TOGGLE_1);  // Read toggle
-
-  bool switchState2 = digitalRead(BUTTON_PIN_2);     // Read switch
-  bool toggleState2 = digitalRead(BUTTON_TOGGLE_2);  // Read toggle
-
   if (switchState1 == LOW)
   {
     Serial.printf("clicked button %d: %d\n", switchState1, toggleState1);
@@ -43,11 +42,14 @@ void loop()
     send_command();
   }
 
+  bool switchState2 = digitalRead(BUTTON_PIN_2);     // Read switch
+  bool toggleState2 = digitalRead(BUTTON_TOGGLE_2);  // Read toggle
+
   if (switchState2 == LOW)
   {
     Serial.printf("clicked button %d: %d\n", switchState2, toggleState2);
-    command.button_number = switchState2;
-    command.pin_position  = toggleState2;
-    send_command();
+    //   command.button_number = switchState2;
+    //   command.pin_position  = toggleState2;
+    //   send_command();
   }
 }
